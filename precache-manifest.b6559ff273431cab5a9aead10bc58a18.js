aa7125ba3574066d912ad0e97160357352789433
Esar Behlum <esarnb@gmail.com>
2019-07-25 14:32:57 -0700

    Updates

---
+++ precache-manifest.b6559ff273431cab5a9aead10bc58a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb17b170e67a354f2279f69f35f792a",
+    "url": "/memory-card-clicker/index.html"
+  },
+  {
+    "revision": "eebbc6f7af198eb47a5a",
+    "url": "/memory-card-clicker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/memory-card-clicker/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eebbc6f7af198eb47a5a",
+    "url": "/memory-card-clicker/static/js/main.45aff656.chunk.js"
+  },
+  {
+    "revision": "a18e1fc4a7905a709fc6",
+    "url": "/memory-card-clicker/static/js/runtime~main.427c719f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-card-clicker/static/media/logo.5d5d9eef.svg"
+  }
+]);
