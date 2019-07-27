1982eb0485199703751a7786beebbbdd3a1cffd9
Esar Behlum <esarnb@gmail.com>
2019-07-26 23:41:12 -0700

    fixing image path 1

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,18 +6,8 @@ function Nav(props) {
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">Memory Card Clicker</a>
-      <span style={{float: "right"}}>Points: {props.currentScore} | High Score: {props.highScore} </span>
+      <span className="moveRight">Points: {props.currentScore} | High Score: {props.highScore} </span>
     </nav>
-
-    //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    //     <span className="navbar-toggler-icon"></span>
-    //   </button>
-    //   <div className="collapse navbar-collapse" id="navbarText">
-    //     <span className="navbar-text">
-    //       Points: {props.points} | High Score: {props.highscore} 
-    //     </span>
-    //   </div>
-    // </nav>
   )
 }
 
